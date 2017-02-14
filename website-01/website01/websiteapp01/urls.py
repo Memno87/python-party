addfd984c8bfbe080b4b31292cfa2e44ab158b84
Leo <leofrancisthornton@gmail.com>
2017-02-14 19:04:13 +0000

    additional 'details' page added

--- website-01/website01/websiteapp01/urls.py
+++ website-01/website01/websiteapp01/urls.py
@@ -5,4 +5,5 @@ from websiteapp01 import views
 urlpatterns = [
 	url(r'^$', views.HomePageView.as_view()),
 	url(r'^about/$', views.AboutPageView.as_view()),
+	url(r'^detail/$', views.DetailPageView.as_view()),
 ]
--- website-01/website01/websiteapp01/views.py
+++ website-01/website01/websiteapp01/views.py
@@ -4,10 +4,18 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+#class HomePageView(TemplateView):
+		#def get(self, request, **kwargs):
+			#return render(request, 'index.html', context=None)
+
+# Homepage alternative
 class HomePageView(TemplateView):
-		def get(self, request, **kwargs):
-			return render(request, 'index.html', context=None)
+	template_name = "index.html"
 
 # About screen view
 class AboutPageView(TemplateView):
 	template_name = "about.html"
+
+# Further details page
+class DetailPageView(TemplateView):
+	template_name = "detail.html"
